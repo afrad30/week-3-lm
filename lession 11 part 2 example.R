efbadec6b090b5b1e0a6ab84ef37ef39a9ed80c4
Afrad30 <afradhossain30@gmail.com>
2024-03-14 13:54:55 +0600

    14/3

--- lession 11 part 2 example.R
+++ lession 11 part 2 example.R
@@ -5,31 +5,14 @@ any(is.na(OME)) # check for missing values
 dat = subset(OME, OME != "N/A") # manually remove OME missing values identified with "N/A"
 dat$OME = factor(dat$OME)
 dat
-summary(dat)
 
-plot(dat$Age, dat$Correct / dat$Trials )
-plot(dat$OME, dat$Correct / dat$Trials )
-plot(dat$Loud, dat$Correct / dat$Trials )
-plot(dat$Noise, dat$Correct / dat$Trials )
-
-#glm (generalized linear model), family = binomial() for logistic regression, family = poisson() for Poisson regression, etc.
-
-mod_glm = glm(Correct/Trials ~ Age + OME + Loud + Noise, data=dat, weights=Trials, family="binomial")
-plot(mod_glm)
-summary(mod_glm)
-
-plot(residuals(mod_glm, type="deviance"))
-plot(fitted(mod_glm), dat$Correct/dat$Trials)
-
-#X = model.matrix(mod_glm)[,-1]# -1 removes the column of 1s for the intercept
-#X
 library("rjags")
 library("coda")
 
 mod_string3 = " model {
-	for (i in 1:length(y)) {
+	for (i in 1:length(n)) {
 		y[i] ~ dbin(phi[i], n[i])
-		logit(phi[i]) = a*ID + b[1]*Age[i] + b[2]*OMElow[i] + b[3]*Loud[i] + b[4]*Noiseincoherent[i]
+		logit(phi[i]) = a*id + b[1]*Age[i] + b[2]*OME[i] + b[3]*Loud[i] + b[4]*Noise[i]
 	}
 
 	a ~ dnorm(mu, tau)
@@ -43,8 +26,7 @@ mod_string3 = " model {
 	
 } "
 
-data_jags3 = as.list(as.data.frame(X))
-data_jags3$y = dat$Correct # this will not work if there are missing values in dat (because they would be ignored by model.matrix). Always make sure that the data are accurately pre-processed for JAGS.
+data_jags3 =list(y=dat$Correct, id=dat$ID, Age=dat$Age, OME=as.numeric(dat$OME), Loud=dat$Loud, Noise=as.numeric(dat$Noise) )
 data_jags3$n = dat$Trials
 str(data_jags3)# make sure that all variables have the same number of observations (712).
 
